227db2902689c4997a67b514e93daf392bbbc30a
Razo <raz.miqayelyan@gmail.com>
2022-04-30 09:33:04 +0400

    first commit

---
+++ index.js
@@ -0,0 +1,35 @@
+import mysql from "mysql";
+import express from 'express';
+import 'dotenv/config'
+
+let connection = mysql.createConnection({
+    host     : process.env.HOST,
+    user     : process.env.USER,
+    password : process.env.PASSWORD,
+    database : process.env.DATABASE
+  });
+   
+connection.connect();
+
+const app = express()
+
+app.get('/', (req, res) => {
+    connection.query("SELECT * FROM Users WHERE username = 'razmikayelyan'", function (error, results, fields) {
+        if (error) throw error;
+        res.send(results)
+      });
+       
+})
+
+app.get('/values', (req, res) => {
+    var sql = "INSERT INTO Users (username, email, password) VALUES ('newuser', 'newuser@gmail.com', '123456')";
+    connection.query(sql, function (err, result) {
+      if (err) throw err;
+     res.send("OK");
+    });
+})
+
+app.listen(6969 , (ok, err) =>{
+    if(err) connection.end();
+})
+
