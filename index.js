1daca1aab78393bec2baa389947d305bcb0e5279
Razo <raz.miqayelyan@gmail.com>
2022-05-08 22:42:00 +0400

    make it better

--- index.js
+++ index.js
@@ -16,18 +16,15 @@ const __dirname = path.dirname(__filename);
 
 app.use(express.json())
 app.use(express.urlencoded())
-app.use(express.static(__dirname + "/public"))
-
-
 const myLogger = function (req, res, next) {
-  if(req.method !== 'POST' || priv === true){
-    res.redirect('/index.html')
-  }else{
-    next()
+  if(req.method !== 'POST' && priv === true){
+    req.url = '/'
   }
+  next()
 }
 
 app.use(myLogger)
+app.use(express.static(__dirname + "/public"))
 
 
 let connection = mysql.createConnection({
