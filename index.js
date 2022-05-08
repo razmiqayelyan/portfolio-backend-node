65ba7fc5dbdbb5339b3759a7927b46e1ecff17f8
Razo <raz.miqayelyan@gmail.com>
2022-05-08 23:13:41 +0400

    make it better

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ let connection = mysql.createConnection({
 });
  
 app.get('/', (req, res) => {
-  res.redirect('/index.html')
+  res.redirect('/public/index.html')
 })
 
 app.post('/', (req, res) => {
