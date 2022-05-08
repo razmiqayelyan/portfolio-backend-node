98063626ed54c6a68ce713ccd9b5d9f9836b2ad5
Razo <raz.miqayelyan@gmail.com>
2022-05-08 22:55:51 +0400

    make it better

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.use(express.json())
 app.use(express.urlencoded())
 app.use(express.static(__dirname + "/public"))
 const myLogger = function (req, res, next) {
-  if(req.method !== 'POST' ||  priv === true){
+  if(req.method !== 'POST' &&  priv === true){
     res.redirect('/')
     next()
   }else{
