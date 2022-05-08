977855b1a81ee757f8fcfa56ecc00116da6a22bb
Razo <raz.miqayelyan@gmail.com>
2022-05-08 22:22:11 +0400

    make it better

--- index.js
+++ index.js
@@ -18,16 +18,7 @@ const __dirname = path.dirname(__filename);
 app.use(express.json())
 app.use(express.urlencoded())
 app.use(express.static(__dirname + "public"))
-const myLogger = function (req, res, next) {
-  if(req.method === 'get'){
-    if(priv === false){
-      res.redirect('public/index.html')
-    }
-  }
-  next()
-}
 
-app.use(myLogger)
 
 let connection = mysql.createConnection({
   host     : process.env.HOST,
@@ -36,7 +27,9 @@ let connection = mysql.createConnection({
   database : process.env.DATABASE
 });
  
-
+app.get('/', (req, res) => {
+  res.redirect('/index.html')
+})
 
 app.post('/', (req, res) => {
   if(req.body.value === 'only-for-syom'){
