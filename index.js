13f847e9a8fcd37fbacb4cbccb8a7db56504deac
Razo <raz.miqayelyan@gmail.com>
2022-05-08 23:45:46 +0400

    gdfgasdsdada

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const __filename = fileURLToPath(import.meta.url);
 
 const __dirname = path.dirname(__filename);
 
-app.use('/public', express.static(__dirname + "public"))
+app.use('/public', express.static(__dirname + "/public"))
 
 let connection = mysql.createConnection({
   host     : process.env.HOST,
