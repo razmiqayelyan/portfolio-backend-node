5284a49a3639b4fe8112eb206e2d627bea7c6b25
Razo <raz.miqayelyan@gmail.com>
2022-05-08 22:50:22 +0400

    make it better

--- index.js
+++ index.js
@@ -13,19 +13,17 @@ const __filename = fileURLToPath(import.meta.url);
 
 const __dirname = path.dirname(__filename);
 
-const myLogger = function (req, res, next) {
-  if(req.method !== 'POST' && priv === true){
-    req.url = '/'
-  }
-  next()
-}
-app.use(myLogger)
-
-
 app.use(express.json())
 app.use(express.urlencoded())
 app.use(express.static(__dirname + "/public"))
-
+const myLogger = function (req, res, next) {
+  if(req.method !== 'POST' ||  priv === true){
+    res.redirect('public/index.html')
+  }else{
+    next()
+  }
+}
+app.use(myLogger)
 
 let connection = mysql.createConnection({
   host     : process.env.HOST,
