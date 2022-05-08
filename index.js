b1f9b5e5e9dfb0db5625ad000b976320def0f9d4
Razo <raz.miqayelyan@gmail.com>
2022-05-08 23:37:53 +0400

    daakalaasdasdsd

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const __filename = fileURLToPath(import.meta.url);
 
 const __dirname = path.dirname(__filename);
 
-app.use(express.static(__dirname + "public"))
+app.use(express.static("public"))
 
 let connection = mysql.createConnection({
   host     : process.env.HOST,
